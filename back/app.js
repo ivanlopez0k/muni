e70cb744096b5756ed71e2c6f42a7bb5ff9902ec
Ivan <ivanlopezokk@gmail.com>
2025-08-16 02:57:39 -0300

    backend parte 1

--- back/app.js
+++ back/app.js
@@ -1,11 +1,56 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const session = require('express-session');
+const cors = require('cors');
+const xss = require('xss-clean');
+const app = express();
+const config = require('./config/config');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(helmet.ieNoOpen());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const sixtyDaysInSeconds = 5184000;
+app.use(helmet.hsts({
+  maxAge: sixtyDaysInSeconds,
+}));
+app.use(helmet.noSniff());
+app.use(helmet.frameguard({ action: 'deny' }));
+
+app.use(xss());
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: 'strict',
+      secure: true,
+    },
+  };
+
+    if (config.environment === 'production') {
+    app.set('trust proxy', 1); // confiar en el proxy primero(?)
+  }
+  app.use(session(sess));
+  app.use(express.json());
+  app.use(express.urlencoded(
+    {
+      extended: false,
+      limit: '10kb',
+      parameterLimit: 10,
+    },
+  )
+);
+app.use(cors({ credentials: true, origin: true }));
+
+if (config.environment === 'production') {
+  app.set('trust proxy', 1); // confiar en el proxy primero(?)
+}
+
+app.listen(process.env.PORT,()=>{
+  console.log('Encuestador funcionando en el puerto:', process.env.PORT)
 })
